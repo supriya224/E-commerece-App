7f3db68323f01414910defeaef85811351b08700
supriya224 <52038704+supriya224@users.noreply.github.com>
2023-10-24 00:49:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import * as React from 'react';
 import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-import SignUp from './components/SignUp';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './components/SignUp';
+import Navbar from './pages/Navbar';
+import Cart from './pages/Cart';
+import Products from './pages/products';
 
-// import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className=''> 
-  
-      <BrowserRouter>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
-          {/* <Route path="/home" element={<Home />} /> */}
-        </Routes>
-      </BrowserRouter>
-      <div className=''>
-      <Home />
-      </div>
+  <>
+<Navbar/>
+   <Routes>
+    <Route path="/home" element={<Home/>}></Route>
+    <Route path="/products" element={<Products/>}></Route>
+    <Route path="/cart" element={<Cart/>}></Route>
+    <Route path="/login" element={<Login/>}></Route>
+    <Route path="/signup" element={<Signup/>}></Route>
+   
+   </Routes>
+
+
+      </>
     
-    </div>
+   
   );
 }
 //          
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import Navbar from './Navabr';
+import React, { useEffect} from 'react';
+
 function Home() {
+ 
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch('https://flipkart-email-mock.vercel.app/');
+      const data = await res.json();
+      console.log(data);
+    
+    };
+    fetchProducts();
+  });
   return (
-    <div>
-        <Navbar/>
-    </div>
+    <>
+      <section className='mt-2'>
+      
+        <section className='m-auto container bg-blue-900 w-[100%] h-[100vh] text-white'>
+        
+        </section>
+      </section>
+    </>
   );
 }
 
